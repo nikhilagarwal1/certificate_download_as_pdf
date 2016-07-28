9855b61a235d1c80e19fc11367433754abaad577
Nikhil Agarwal - Network <C238664@ema.LILLY.com>
2016-07-28 17:53:44 +0530

    Updated

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,12 +1,12 @@
 class TrainingsController < ApplicationController
 
-	require 'erb'
+  require 'erb'
 
-	def index
-		@agarwal = {abc: "sandhya", cde: "shree"}
-	end
+  def index
+    @agarwal = {abc: "sandhya", cde: "shree"}
+  end
 
-	def get_binding
+  def get_binding
     binding
   end
 
@@ -15,29 +15,29 @@ class TrainingsController < ApplicationController
   end
 
   def certificate
-  	controller =TrainingsController.new
-		controller.index
-  	erb_file = 'app/views/trainings/certificate.html.erb'
-		file_name = File.basename(erb_file, '.erb')
-		html_file = "app/views/trainings/nikhil.html"
-		@agarwal = controller.index
-		erb_str = File.read(erb_file)
-		renderer = ERB.new(erb_str)
-		result = renderer.result(controller.get_binding)
-		File.open(html_file, 'w') do |f|
-  		f.write(result)
-		end
+    controller =TrainingsController.new
+    controller.index
+    erb_file = 'app/views/trainings/certificate.html.erb'
+    file_name = File.basename(erb_file, '.erb')
+    html_file = "app/views/trainings/nikhil.html"
+    @agarwal = controller.index
+    erb_str = File.read(erb_file)
+    renderer = ERB.new(erb_str)
+    result = renderer.result(controller.get_binding)
+    File.open(html_file, 'w') do |f|
+      f.write(result)
+    end
   end
 
 
   def download
-  	output = render_to_string "nikhil.html"
+    output = render_to_string "nikhil.html"
     kit = PDFKit.new(output)
     pdf = kit.to_file 'public/data/my.pdf'
-  	send_file(
+    send_file(
     "#{Rails.root}/public/data/my.pdf",
     filename: "certificate.pdf",
     type: "application/pdf"
-  	)
+    )
   end
 end
